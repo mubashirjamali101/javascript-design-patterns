a12028132fa9b84bf8f0538e9c08fc17bab7d3c1
Mubashir Jamali <mubashirjamali101@gmail.com>
2021-06-04 23:35:08 +0500

    singelton pattern

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+let instance = null
+
 class Car {
     constructor(doors, engine, color) {
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;
+        if (!instance) {
+            this.doors = doors;
+            this.engine = engine;
+            this.color = color;
+            instance = this;
+        } else {
+            return instance;
+        }
     }
 }
 
-class SUV extends Car {
-    constructor(doors, engine, color) {
-        super(doors, engine, color);
-        this.wheels = 4;
+// class SUV extends Car {
+//     constructor(doors, engine, color) {
+//         super(doors, engine, color);
+//         this.wheels = 4;
 
-    }
-}
+//     }
+// }
 
 const civic = new Car(4, 'V6', 'grey');
-const cx5 = new SUV(4, "V8", 'reg')
+const honda = new Car(2, "V8", 'reg')
 
-console.log(cx5)
+console.log(civic)
+console.log(honda)
