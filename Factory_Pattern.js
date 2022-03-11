ceab8f7e3475d614e5e5d319fd3d001f5ecbcfc1
Mubashir Jamali <38682421+mubashirjamali101@users.noreply.github.com>
2022-03-11 18:44:54 +0500

    Added comments in factory pattern.

--- Factory_Pattern.js
+++ Factory_Pattern.js
@@ -1,3 +1,4 @@
+// make a class for the object, that you want to generate over-and-over again.
 class Car {
     constructor(doors, engine, color) {
         this.doors = doors;
@@ -7,6 +8,8 @@ class Car {
     }
 }
 
+
+// then make a class for the factory that will generate the desired object at scale for you.
 class carFactory {
     createCar(type) {
         switch (type) {
@@ -18,7 +21,11 @@ class carFactory {
     }
 }
 
+// make an instace of the factory first.
 const factory = new carFactory();
+
+// then make the desired object using the instance of factory.
 const myHonda = factory.createCar('honda');
 
+// then do whatever you are required to do with that object.
 console.log(myHonda)
