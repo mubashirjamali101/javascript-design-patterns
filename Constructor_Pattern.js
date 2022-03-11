f002f1f9986dada06c590a8602d37592e2d3244a
Mubashir Jamali <38682421+mubashirjamali101@users.noreply.github.com>
2022-03-11 18:50:04 +0500

    added name property, printed objects and modified comments.

--- Constructor_Pattern.js
+++ Constructor_Pattern.js
@@ -1,18 +1,24 @@
-class Car {//Base class as blue print
-    constructor(doors, engine, color) {
+// Base class as blue print
+class Car {
+    constructor(name, doors, engine, color) {
+        this.name = name;
         this.doors = doors;
         this.engine = engine;
         this.color = color;
     }
 }
 
-class SUV extends Car {//SUv class extending class Car
-    constructor(doors, engine, color) {
-        super(doors, engine, color);
+// SUV class extending class Car
+class SUV extends Car {
+    constructor(name, doors, engine, color) {
+        super(name, doors, engine, color);
         this.wheels = 4;
-
     }
 }
 
-const civic = new Car(4, 'V6', 'grey');//logging new object civic
-const cx5 = new SUV(4, "V8", 'reg');//logging new object cx5
+// make a new car and a new SUV
+const civic = new Car("Honda Civic 2022", 4, "V6", "grey");
+const cx5 = new SUV("cx5", 4, "V8", "reg");
+
+// and then print both
+console.log(`${civic.name} = ${civic} \n${cx5.name} = ${cx5}`)
